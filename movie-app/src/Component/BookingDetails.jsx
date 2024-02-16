70c8f1e536db4a199c0944e0ce9b5fccad66c801
Dibakardebnath <ddibakar190@gmail.com>
2024-02-16 23:58:06 +0530

    doe

--- movie-app/src/Component/BookingDetails.jsx
+++ movie-app/src/Component/BookingDetails.jsx
@@ -1,9 +1,106 @@
+import { useState } from "react";
+import './BookingDetails.css';
 
 
 export const BookingDetails=()=>{
-    return (
-        <div>
-            Booking deteail
-        </div>
-    )
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        // You can perform further actions with the form data here
+        console.log(formData);
+      };
+
+    /*   <form onSubmit={handleSubmit}>
+         
+            <label htmlFor="firstName">First Name:</label>
+            <br />
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+            />
+      
+            <label htmlFor="lastName">Last Name:</label><br />
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+            />
+          <br />
+       
+            <label htmlFor="email">Email:</label><br />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          
+          <button type="submit">Submit</button>
+        </form> */
+    
+      
+        return (
+            <div >
+                <h1>Seating Arrangment</h1>
+
+                <div className="Booking-div">
+                <div className="first-div">
+                    <h1>Ticket Booking Form</h1>
+                    <form className="form-container">
+                <label htmlFor="fname">Enter Your Name</label>
+                <input className="input-text" type="text" id="fname" name="firstname" placeholder="Your name.." />
+          
+                <label htmlFor="lname">Enter Date</label>
+                <input className="input-text" type="date" id="lname" name="lastname" placeholder="Your last name.." />
+          
+                <label htmlFor="country">Select Movie Time</label>
+                <select className="select-text" id="country" name="country">
+                  <option value="australia">09:00 AM</option>
+                  <option value="canada">12:00 PM</option>
+                  <option value="usa">03:00PM</option>
+                  <option value="usa">06:00PM</option>
+                </select>
+
+                <label htmlFor="country">Select No.of Ticket</label>
+                <select className="select-text" id="country" name="country">
+                  <option value="australia">09:00 AM</option>
+                  <option value="canada">12:00 PM</option>
+                  <option value="usa">03:00PM</option>
+                  <option value="usa">06:00PM</option>
+                </select>
+          
+                <input className="button" type="submit" value="Book Ticket" />
+              </form>
+                </div>
+
+                <div className="second-div">
+                   all the seates
+                </div>
+                </div>
+              
+             
+            </div>
+          );
+          
+    
+    
 }
--- movie-app/src/Component/Movies.jsx
+++ movie-app/src/Component/Movies.jsx
@@ -1,33 +1,93 @@
 import { useEffect, useState } from "react";
-import './Movies.css'
+import "./Movies.css";
+import axios from 'axios';
 
-export const Movies=()=>{
-    const images = [
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKyUbqgVPThBXpaXixpTV51Rlvu82uJaCXvw&usqp=CAU',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT8gsKQJAwV50GuIr6cERxIqJeC0yCGn8P1g&usqp=CAU',
-        ''
-       
-      ];
-    const [currentIndex, setCurrentIndex] = useState(0);
+export const Movies = () => {
+  const [data,setData]=useState([])
+  console.log(data)
+  // ..................Slider............................        848f6118caa493508c0526a82189f5ab
+  const sliderImages = [
+    "https://wowslider.com/sliders/demo-74/data1/images/newyorkcity336475_1280.jpg",
+    "https://startit.qodeinteractive.com/wp-content/uploads/2015/10/image-slider-1-home-main.jpg",
+    "https://wowslider.com/sliders/demo-74/data1/images/newyorkcity336475_1280.jpg",
+    "https://startit.qodeinteractive.com/wp-content/uploads/2015/10/image-slider-4-home-main.jpg",
+    "https://wowslider.com/sliders/demo-74/data1/images/newyorkcity336475_1280.jpg",
+    "https://startit.qodeinteractive.com/wp-content/uploads/2015/10/image-slider-2-home-main.jpg",
+    "https://wowslider.com/sliders/demo-74/data1/images/newyorkcity336475_1280.jpg",
+    "https://wowslider.com/sliders/demo-74/data1/images/newyorkcity336475_1280.jpg",
+    "https://wowslider.com/sliders/demo-74/data1/images/london441853_1280.jpg",
+  ];
+  // const [autoSlide, setAutoSlide] = useState(true);
+  // const [slideInterval, setSlideInterval] = useState(3000);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-      }, 3000); // Change image every 3 seconds
-  
-      return () => clearInterval(intervalId);
-    }, [images.length]);
-  
-    return (
-      <div className="slider-container">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            className={`slider-image ${index === currentIndex ? 'active' : ''}`}
-            src={image}
-            alt={`Slide ${index}`}
-          />
-        ))}
-      </div>
+  useEffect(() => {
+    let interval = setInterval(() => {
+      goToNextSlide();
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []); // No dependency here, as we only want to run once on mount
+
+  const goToPreviousSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? sliderImages.length - 1 : prevIndex - 1
     );
-}
+  };
+
+  const goToNextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === sliderImages.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+
+  useEffect(()=>{
+       fetchData();
+  },[])
+  const fetchData=async()=>{
+    try {
+      await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=848f6118caa493508c0526a82189f5ab")
+      .then((res)=>{
+      setData(res.data.results.slice(0, 12))
+      // console.log(res.data.results)
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div>
+      <div className="slider-container">
+        <img className="slide" src={sliderImages[currentIndex]} />
+      </div>
+      <div className="slider-button">
+        <button className="slider-button1" onClick={goToPreviousSlide}>
+          &#10094;
+        </button>
+        <button className="slider-button2" onClick={goToNextSlide}>
+          &#10095;
+        </button>
+      </div>
+      <div><h1>Movies</h1></div>
+<div className="parant-container">
+{data.map((ele)=>
+  
+  <div key={ele.id} className="movie-container">
+   <img style={{borderRadius:"10px",width:"200px"}}  src={`https://image.tmdb.org/t/p/w500${ele.poster_path}`} alt="" />
+   <div className="vote-container">
+   <p> <i style={{ color: '#dc344b' }} class="fa-solid fa-star"></i> {Number(ele.vote_average.toFixed(1))}/10</p>
+
+    <p>{ele.vote_count} Votes</p>   
+    </div>
+  </div>
+   )}
+</div>
+ 
+
+
+
+    </div>
+  );
+};
